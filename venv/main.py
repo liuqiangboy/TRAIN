288f632e2a8d61981dc9ea8d554c4b9d08356a22
qiang <770422341@qq.com>
2018-12-11 16:59:52 +0800

    the version with __main__

--- venv/main.py
+++ venv/main.py
@@ -2,12 +2,12 @@ def calc_ent(x):
     """
         calculate shanno ent of x
     """
-
     x_value_list = set([x[i] for i in range(x.shape[0])])
     ent = 0.0
     for x_value in x_value_list:
         p = float(x[x == x_value].shape[0]) / x.shape[0]
         logp = np.log2(p)
         ent -= p * logp
-
     return ent
+if __name__=='__main__':
+    print'hello world'
